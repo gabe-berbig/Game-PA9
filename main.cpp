6152563d37f6e95630892be339e2e0ce7cd11ae4
GOMiCHO <chowgomi@gmail.com>
2016-04-20 19:17:06 -0700

    Update main.cpp
    Made some changes. Feel free to change as you want if you can make it work better.

--- main.cpp
+++ main.cpp
@@ -2,37 +2,37 @@
 
 int main() {
 	sf::RenderWindow window(sf::VideoMode(1000, 800), "Game [In Developement]");
-
 	sf::RectangleShape player(sf::Vector2f(20, 20));
 	player.setFillColor(sf::Color::Red);
 	
-	sf::Image player_img;
-	if (!player_img.loadFromFile("p_img.png"))	//For Later
-		return 1;
-	sf::Sprite Sprite;
+	//sf::Image player_img;
+	//if (!player_img.loadFromFile("p_img.png"))	//For Later
+	//	return 1;
+	//sf::Sprite Sprite;
 
-	int x = 0, y = 0, speed = 2;
-	player.setPosition(300, 300);
+	int speed = 30;	//Speed of Player
+	player.setPosition(500, 300);	//Spawning Point
 	while (window.isOpen()) {
 		sf::Event event;
 		while (window.pollEvent(event)) {
 			if (event.type == sf::Event::Closed)
 				window.close();
 			if (event.type == sf::Event::KeyPressed) {
-				if (event.key.code == sf::Keyboard::W) // move up
-					player.move(x * speed, (--y * speed));
-				if (event.key.code == sf::Keyboard::S) // move down
-					player.move(x * speed, (++y * speed));
-				if (event.key.code == sf::Keyboard::D) // move right
-					player.move((++x * speed), y * speed);
-				if (event.key.code == sf::Keyboard::A) // move left
-					player.move((--x * speed), y * speed);
+				if (event.key.code == sf::Keyboard::W) // Move Up
+					player.move(0, -speed);
+				if (event.key.code == sf::Keyboard::S) // Move Down
+					player.move(0, speed);
+				if (event.key.code == sf::Keyboard::D) // Move Right
+					player.move(speed, 0);
+				if (event.key.code == sf::Keyboard::A) // Move Left
+					player.move(-speed, 0);
+				if (event.key.code == sf::Keyboard::Escape)	// Press Esc to Exit
+					window.close();
 			}
 		}
 		window.clear();
 		window.draw(player);
 		window.display();
 	}
-
 	return 0;
 }
